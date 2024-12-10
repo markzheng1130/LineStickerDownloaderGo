ce0f40be2e7327693c5721fbc45fdd3c2bdf7668
Mark Cheng <mark_cheng@trendmicro.com>
2024-12-10 19:16:37 +0800

    add

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module line-sticker-downloader-go
 
 go 1.22.6
+
+require go.mongodb.org/mongo-driver v1.17.1
--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"line-sticker-downloader-go/stickerhandler"
+	"line-sticker-downloader-go/mongodb_handler"
 )
 
 func main() {
-	webSourceUrl := "https://store.line.me/stickershop/product/21802595/en" // type=static, url=staticUrl
-	// webSourceUrl := "https://store.line.me/stickershop/product/12378/zh-Hant" //type=animation, url=animationUrl
-	// webSourceUrl := "https://store.line.me/stickershop/product/7121/en" //type=popup, url=popupUrl
-	// webSourceUrl := "https://store.line.me/stickershop/product/18085/zh-Hant" //type=popup_sound, url=popupUrl
-	// webSourceUrl := "https://store.line.me/emojishop/product/5bc84438031a6704f8cff722/en" //type=static, url=staticUrl
-	// webSourceUrl := "https://store.line.me/stickershop/product/17054/zh-Hant" //type=name, url=staticUrl
+	// webSourceUrl := "https://store.line.me/stickershop/product/21802595/en" // type=static, url=staticUrl
+	// // webSourceUrl := "https://store.line.me/stickershop/product/12378/zh-Hant" //type=animation, url=animationUrl
+	// // webSourceUrl := "https://store.line.me/stickershop/product/7121/en" //type=popup, url=popupUrl
+	// // webSourceUrl := "https://store.line.me/stickershop/product/18085/zh-Hant" //type=popup_sound, url=popupUrl
+	// // webSourceUrl := "https://store.line.me/emojishop/product/5bc84438031a6704f8cff722/en" //type=static, url=staticUrl
+	// // webSourceUrl := "https://store.line.me/stickershop/product/17054/zh-Hant" //type=name, url=staticUrl
 
-	h := stickerhandler.New(webSourceUrl)
-	h.Run()
+	// h := sticker_handler.New(webSourceUrl)
+	// h.Run()
+
+	mongodbUrl := ""
+	m := mongodb_handler.New(mongodbUrl)
+	m.TestRun()
 }
